efafc3e085aa4b02eb7cd1aec458a68589cbdc4f
TainaDeAlves <tainadesouzalves@gmail.com>
2024-08-21 15:39:19 -0300

    .

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -19,8 +19,8 @@
       href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@400;500;700&display=swap"
       rel="stylesheet"
     />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
-    <link rel="stylesheet" href="{{asset("css/main.css")}}"/>
   </head>
 
   <body>
